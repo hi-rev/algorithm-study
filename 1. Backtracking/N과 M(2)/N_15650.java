fcb972ea693460bb9da0afdc94951afb34191e23
ijun <ljun1118@ks.ac.kr>
2022-08-22 21:50:12 +0900

    이해완료

--- 1. Backtracking/N과 M(2)/N_15650.java
+++ 1. Backtracking/N과 M(2)/N_15650.java
@@ -1,5 +1,41 @@
-public class N_15650 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class N_15650 {
+
+    public static int[] arr;
+    public static boolean[] visit;
+
+    public static int N;
+    public static int M;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+
+        visit = new boolean[N];
+        arr = new int[M];
+
+        solution(1, 0);
+    }
+
+    public static void solution(int pos, int depth) {
+
+        //깊이가 M과 같아지면 출력
+        if (depth == M) {
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+
+            //전 상태로 돌아감
+            return;
+        }
+
+        //현재 위치부터 반복
+        for (int i = pos; i <= N; i++) {
+            arr[depth] = i;
+            solution(i + 1, depth + 1);
+        }
     }
 }
