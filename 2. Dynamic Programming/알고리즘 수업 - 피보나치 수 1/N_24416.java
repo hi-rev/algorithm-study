fc8bc169a8c8f64eccb76016b2b3aaedb64dca3d
hi-rev <hirev05@gmail.com>
2022-09-06 15:41:39 +0900

    알고리즘 수업 - 피보나치 수 1

--- 2. Dynamic Programming/알고리즘 수업 - 피보나치 수 1/N_24416.java
+++ 2. Dynamic Programming/알고리즘 수업 - 피보나치 수 1/N_24416.java
@@ -4,38 +4,40 @@ import java.util.Scanner;
 // n의 피보나치 수 구하기
 // 피보나치 수란? 첫째 및 둘째 항이 1이며 그 뒤 모든 항은 바로 앞 두 항의 합인 수열
 public class N_24416 {
-    static Scanner sc = new Scanner(System.in);
-    static int n = sc.nextInt();
-    static int[] f = new int[n];
     static int cnt1 = 0;
     static int cnt2 = 0;
+    static int[] f;
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        f = new int[n];
+
         fib(n);
         fibonacci(n);
-        System.out.print(cnt1 + " ");
+        System.out.println(cnt1);
         System.out.println(cnt2);
     }
 
     // 피보나치 수 재귀
     static int fib(int n) {
-        cnt1++;
-        if (n == 0 || n == 1) {
+        if (n == 1 || n == 2) {
+            cnt1++;
             return 1;
-        } else {
-            return (fib(n - 1) + fib(n - 2));
         }
+        return (fib(n - 1) + fib(n - 2));
     }
 
     // 피보나치 수 DP(동적 프로그래밍)
     // 동적 계획법에서는 반복되는 계산을 막기 위해 이전에 계산했던 내용을 배열에 저장한다.
     // 대표적으로는 top-down과 bottom-up이 있다고 한다.
     static int fibonacci(int n) {
-        cnt2++;
         f[0] = 1;
         f[1] = 1;
+
         for (int i = 2; i < n; i++) {
-            f[i] = f[i-1] + f[i-2];
+            cnt2++;
+            f[i] = f[i-2] + f[i-1];
         }
-        return f[n];
+        return f[n-1];
     }
 }
