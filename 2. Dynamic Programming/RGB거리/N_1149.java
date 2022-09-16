d90526208e7a42da1910dadd1c73397cec5548f3
hi-rev <hirev05@gmail.com>
2022-09-16 17:28:30 +0900

    🚧 진행중

--- 2. Dynamic Programming/RGB거리/N_1149.java
+++ 2. Dynamic Programming/RGB거리/N_1149.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class N_1149 {
+    static int[][] cost;
+    static int[][] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 집의 수
+
+        cost = new int[n][3];
+        dp = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            cost[i][0] = sc.nextInt();
+            cost[i][1] = sc.nextInt();
+            cost[i][2] = sc.nextInt();
+        }
+
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
+
+        System.out.println();
+    }
 }
