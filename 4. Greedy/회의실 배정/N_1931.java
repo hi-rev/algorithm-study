8da171289c6e71a4af82d90202e837543013bdb8
hi-rev <hirev05@gmail.com>
2022-10-04 10:53:28 +0900

    회의실 배정

--- 4. Greedy/회의실 배정/N_1931.java
+++ 4. Greedy/회의실 배정/N_1931.java
@@ -1,2 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class N_1931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine()); // 회의의 수
+        int[][] time = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+        // 2차원 배열 정렬
+        Arrays.sort(time, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
+        });
+
+        int cnt = 1;
+        int[] temp = time[0];
+        for (int i = 1; i < n; i++) {
+            if (temp[1] <= time[i][0]) {
+                cnt++;
+                temp = time[i];
+            }
+        }
+        System.out.println(cnt);
+    }
 }
