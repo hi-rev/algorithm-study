cf17791fe2880dbad928cfdf96b4696c5ef769c5
hi-rev <hirev05@gmail.com>
2022-10-07 13:42:58 +0900

    Processing

--- 5. Stack/괄호/N_9012.java
+++ 5. Stack/괄호/N_9012.java
@@ -1,2 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class N_9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            String s = br.readLine();
+            String arr[] = s.split("");
+            int cnt = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j].equals("(")) {
+                    cnt++;
+                } else {
+                    if (cnt == 0) {
+                        cnt = 0;
+                    } else {
+                        cnt--;
+                    }
+                }
+            }
+            if (cnt == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
 }
