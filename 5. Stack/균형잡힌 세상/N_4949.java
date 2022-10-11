ef5b5365d10af5d60f9922203774bfb5edf048df
hi-rev <hirev05@gmail.com>
2022-10-11 15:52:38 +0900

    균형잡힌 세상

--- 5. Stack/균형잡힌 세상/N_4949.java
+++ 5. Stack/균형잡힌 세상/N_4949.java
@@ -1,2 +1,51 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class N_4949 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String s = sc.nextLine();
+            if (s.equals(".")) {
+                break;
+            }
+
+            System.out.println(bracket(s));
+        }
+    }
+
+    static String bracket(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(' || c == '[') {
+                stack.push(c);
+            }
+
+            if (c == ')') {
+                if (stack.empty() || stack.peek() != '(') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            }
+
+            if (c == ']') {
+                if (stack.empty() || stack.peek() != '[') {
+                    return "no";
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        if (stack.empty()) {
+            return "yes";
+        } else {
+            return "no";
+        }
+    }
 }
