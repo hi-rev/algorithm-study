c69e288968ed9e70842673003ac1f05551431b5a
hi-rev <hirev05@gmail.com>
2022-10-24 16:47:20 +0900

    백만 장자 프로젝트

--- SWEA/D2/S_1859.java
+++ SWEA/D2/S_1859.java
@@ -3,28 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class S_1859 {
-    static long answer = 0;
+    static long answer;
+    static int n;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine()); // test case
         for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
+            n = Integer.parseInt(br.readLine());
             String s = br.readLine();
             String[] sArr = s.split(" ");
-            int[] arr = new int[n];
+            arr = new int[n];
             for (int j = 0; j < n; j++) { // 배열 만들기
-                arr[i] = Integer.parseInt(sArr[i]);
+                arr[j] = Integer.parseInt(sArr[j]);
             }
 
-
-            // 배열에서 제일 큰 수를 찾아보자
-            max(arr, n, 0);
-            System.out.println(answer);
+            answer = 0;
+            max(0);
+            System.out.println("#" + (i+1) + " " + answer);
         }
     }
 
-    static void max(int[] arr, int n, int start) {
-
+    static void max(int start) {
         // 가장 큰 값 찾기
         int max = 0;
         int idx = 0;
@@ -35,12 +35,8 @@ public class S_1859 {
             }
         }
 
-        if (start == idx) { // 종료 코드
-            return;
-        }
-
         // 가장 큰값까지 사고 팔기
-        int res = 0;
+        long res = 0;
         int cnt = 0;
         for (int i = start; i < idx; i++) {
             res += arr[i];
@@ -52,7 +48,6 @@ public class S_1859 {
         if (idx == n-1) { // 종료 코드
             return;
         }
-
-        max(arr, n, idx+1);
+        max(idx+1);
     }
 }
